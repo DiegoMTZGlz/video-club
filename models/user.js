0eff6be3440e09a16259a12cc162a70a04d62e0d
Diego Martínez <a353198@uach.mx>
2023-10-29 19:49:41 -0600

    Mod users

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 const schema = mongoose.Schema({
-    __name: String,
+    _name: String,
     _lastName: String,
     _email: String,
     _password: String
